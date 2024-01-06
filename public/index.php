a185bc56a2f494ff67e0407987265e5d4e418d83
Irel Bacolod <kianirel56@gmail.com>
2024-01-06 19:52:10 +0800

    created register api

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Slim\Factory\ServerRequestCreatorFactory;
 
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/connection/db.php';
 
 
 // Instantiate app
--- src/controllers/user.php
+++ src/controllers/user.php
@@ -8,9 +8,47 @@ class User
 {
     public static function handleRequest(Request $request, Response $response): Response
     {
-        $body = $request->getBody()->getContents();
-        $response->getBody()->write($body);
+
+        $sql = "INSERT INTO user (last_name, first_name, email, pass) VALUES (:lastName, :firstName, :email, :pass)";
+
+        $data = $request->getParsedBody();
+
+        if ($data != null) {
+            $lastName = $data["lastName"];
+            $firstName = $data["firstName"];
+            $email = $data["email"];
+            $pass = password_hash($data["pass"], PASSWORD_DEFAULT);
+
+        }
+
+        try {
+
+            $db = new DB();
+            $conn = $db->connect();
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(":lastName", $lastName);
+            $stmt->bindParam(":firstName", $firstName);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":pass", $pass);
+
+
+            $stmt->execute();
+
+            $db = null;
+
+            return $response->withHeader("Content-Type", 'application/json')->withStatus(200);
+        } catch (PDOException $err) {
+            $error = array(
+                "message" => $err->getMessage()
+            );
+
+            $response->getBody()->write(json_encode($error));
+            return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+        }
+
+
         
-        return $response->withHeader("Content-Type", 'application/json')->withStatus(400);
     }
 }
